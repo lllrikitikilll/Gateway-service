311a8f3baf5b92aa4ef6f1e56d43a1516d9d17f4
Aleksey Panin <lllrikitikilll@yandex.ru>
2024-08-07 21:48:28 +0000

    pep8

--- src/app/core/settings.py
+++ src/app/core/settings.py
@@ -3,6 +3,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class ServiceUrl(BaseModel):
+    """Настройки."""
+    
     auth: str
     transaction: str
     verification: str
@@ -10,6 +12,8 @@ class ServiceUrl(BaseModel):
 
 class Settings(BaseSettings):
     # Временный пример как мне испольховать APP_CONFIG__url__auth
+    """Настройки."""
+
     model_config = SettingsConfigDict(
         case_sensitive=False,
         env_nested_delimiter="__",
