4d4d22c3abc2b92744c7cd574d21290a57d7e5c3
Aleksey Panin <lllrikitikilll@yandex.ru>
2024-08-08 18:41:14 +0000

    Перераспределение сервисов

--- src/app/api/auth_router.py
+++ src/app/api/auth_router.py
@@ -2,10 +2,16 @@ from fastapi import APIRouter
 
 from src.app.api.client import HttpxClient
 from src.app.core.settings import settings
-from src.app.schemas.auth_schemas import UserAuth
+from src.app.schemas.auth_schemas import UserAuth, UserRegister
 
 router = APIRouter(tags=["auth"])
 client = HttpxClient(base_url=settings.url.auth)
+
+
+@router.post("/registration/")
+async def registration(user_req: UserRegister) -> dict:
+    """Проксирует запрос на регистрацию."""
+    return await client.post(endpoint="registration/", data=user_req.model_dump())
 
 
 @router.post("/auth/")
--- src/app/api/transaction_router.py
+++ src/app/api/transaction_router.py
@@ -1,21 +1,19 @@
 from fastapi import APIRouter
 
+from src.app.api.auth_router import client as auth_client
 from src.app.api.client import HttpxClient
 from src.app.core.settings import settings
-from src.app.schemas.transaction_schemas import TransactionRequest
+from src.app.schemas.transaction_schemas import ReportRequest, TransactionRequest
 
 router = APIRouter(tags=["transaction"])
 
 transaction_client = HttpxClient(base_url=settings.url.transaction)
-auth_client = HttpxClient(base_url=settings.url.auth)
 
 
 @router.post("/transaction/")
-async def auth(request: TransactionRequest) -> dict:
+async def transaction(request: TransactionRequest) -> dict:
     """Проксирует запрос на оздание транзакции с проверкой токена."""
-    await auth_client.post(
-        endpoint="check_token/", data=request.token.model_dump()
-    )
+    await auth_client.post(endpoint="check_token/", data=request.token.model_dump())
     request.transaction.user_id = request.token.user_id
     transaction_data = request.transaction.model_dump()
     transaction_data["user_id"] = request.token.user_id
@@ -24,3 +22,15 @@ async def auth(request: TransactionRequest) -> dict:
         endpoint="create_transaction/",
         data=transaction_data,
     )
+
+
+@router.post("/report/")
+async def report(request: ReportRequest) -> dict:
+    """Проксирует запрос на список транзакций за период  с проверкой токена."""
+    await auth_client.post(endpoint="check_token/", data=request.token.model_dump())
+    report_data = request.query.model_dump()
+    report_data["user_id"] = request.token.user_id
+    report_data["from_date"] = report_data["from_date"].isoformat()
+    report_data["to_date"] = report_data["to_date"].isoformat()
+
+    return await transaction_client.post(endpoint="get_report/", data=report_data)
