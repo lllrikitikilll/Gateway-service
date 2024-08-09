176f7e7ea3ce48d234768657bb4145d191ec6e0c
Aleksey Panin <lllrikitikilll@yandex.ru>
2024-08-09 12:24:55 +0000

    pep8

--- src/app/api/auth_router.py
+++ src/app/api/auth_router.py
@@ -1,20 +1,20 @@
 from fastapi import APIRouter
 
-from src.app.api.client import HttpxClient
-from src.app.core.settings import settings
+from src.app.api.client import auth_client
 from src.app.schemas.auth_schemas import UserAuth, UserRegister
 
 router = APIRouter(tags=["auth"])
-client = HttpxClient(base_url=settings.url.auth)
 
 
 @router.post("/registration/")
 async def registration(user_req: UserRegister) -> dict:
     """Проксирует запрос на регистрацию."""
-    return await client.post(endpoint="registration/", data=user_req.model_dump())
+    return await auth_client.post(
+        endpoint="registration/", post_data=user_req.model_dump()
+    )
 
 
 @router.post("/auth/")
 async def auth(user_req: UserAuth) -> dict:
     """Проксирует запрос на авторизацию."""
-    return await client.post(endpoint="auth/", data=user_req.model_dump())
+    return await auth_client.post(endpoint="auth/", post_data=user_req.model_dump())
