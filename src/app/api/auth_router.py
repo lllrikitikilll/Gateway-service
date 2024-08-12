31af9dfa40c5cef905a38a38d1b79d83a587b684
lllrikitikilll <lllrikitikilll@yandex.ru>
2024-08-12 21:41:24 +0000

    смена имени параметра

--- src/app/api/auth_router.py
+++ src/app/api/auth_router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException, status
 
-from src.app.api.client import auth_client
+from src.app.client import auth_client
 from src.app.schemas.auth_schemas import UserAuth, UserRegister
 
 router = APIRouter(tags=["auth"])
@@ -10,7 +10,7 @@ router = APIRouter(tags=["auth"])
 async def registration(user_req: UserRegister) -> dict:
     """Проксирует запрос на регистрацию."""
     response = await auth_client.post(
-        endpoint="registration/", post_data=user_req.model_dump()
+        endpoint="registration/", json=user_req.model_dump()
     )
     if response.status_code != status.HTTP_200_OK:
         raise HTTPException(status_code=response.status_code, detail=response.json())
@@ -20,7 +20,7 @@ async def registration(user_req: UserRegister) -> dict:
 @router.post("/auth/")
 async def auth(user_req: UserAuth) -> dict:
     """Проксирует запрос на авторизацию."""
-    response = await auth_client.post(endpoint="auth/", post_data=user_req.model_dump())
+    response = await auth_client.post(endpoint="auth/", json=user_req.model_dump())
     if response.status_code != status.HTTP_200_OK:
         raise HTTPException(status_code=response.status_code, detail=response.json())
     return response.json()
