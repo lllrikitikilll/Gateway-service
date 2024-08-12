f8fa6c40b2e34e45aff0b7a50f5c93c8b86853bb
lllrikitikilll <lllrikitikilll@yandex.ru>
2024-08-12 21:41:07 +0000

    внедрение зависимости

--- src/app/api/transaction_router.py
+++ src/app/api/transaction_router.py
@@ -1,22 +1,22 @@
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, status, Depends
 
-from src.app.api.client import auth_client, transaction_client
-from src.app.schemas.transaction_schemas import ReportRequest, TransactionRequest
+from src.app.client import auth_client, transaction_client
+from src.app.schemas.transaction_schemas import ReportQuery, Transaction
+from src.app.dependency.auth_dependency import check_token_dependency
+from src.app.schemas.auth_schemas import TokenSchema
+
 
 router = APIRouter(tags=["transaction"])
 
 
 @router.post("/transaction/")
-async def transaction(request: TransactionRequest) -> dict:
+async def transaction(
+    request: Transaction,
+    token: TokenSchema = Depends(check_token_dependency)
+) -> dict:
     """Проксирует запрос на оздание транзакции с проверкой токена."""
-    response = await auth_client.post(
-        endpoint="check_token/", json=request.token.model_dump()
-    )
-    if response.status_code != status.HTTP_200_OK:
-        raise HTTPException(status_code=response.status_code, detail=response.json())
-    request.transaction.user_id = request.token.user_id
-    transaction_data = request.transaction.model_dump()
-    transaction_data["user_id"] = request.token.user_id
+    transaction_data = request.model_dump()
+    transaction_data["user_id"] = token.user_id
     transaction_data["operation"] = transaction_data["operation"].value
     transaction_response = await transaction_client.post(
         endpoint="create_transaction/",
@@ -26,15 +26,13 @@ async def transaction(request: TransactionRequest) -> dict:
 
 
 @router.post("/report/")
-async def report(request: ReportRequest) -> dict:
+async def report(
+    request: ReportQuery,
+    token: TokenSchema=Depends(check_token_dependency)
+) -> dict:
     """Проксирует запрос на список транзакций за период  с проверкой токена."""
-    response = await auth_client.post(
-        endpoint="check_token/", json=request.token.model_dump()
-    )
-    if response.status_code != status.HTTP_200_OK:
-        raise HTTPException(status_code=response.status_code, detail=response.json())
-    report_data = request.query.model_dump()
-    report_data["user_id"] = request.token.user_id
+    report_data = request.model_dump()
+    report_data["user_id"] = token.user_id
     report_data["from_date"] = report_data["from_date"].isoformat()
     report_data["to_date"] = report_data["to_date"].isoformat()
 
