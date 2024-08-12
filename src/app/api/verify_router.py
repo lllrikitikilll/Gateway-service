a706b2888c62d9338f9920a581b0fa97de20cc32
lllrikitikilll <lllrikitikilll@yandex.ru>
2024-08-12 22:54:44 +0000

    отправка токена с id вместе с файлом

--- src/app/api/verify_router.py
+++ src/app/api/verify_router.py
@@ -1,27 +1,30 @@
 import httpx
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, status, Depends, UploadFile, Form
 
 from src.app.core.settings import settings
 from src.app.schemas.verify_schemas import ReportRequest
+from src.app.client import auth_client
+from src.app.dependency.auth_dependency import check_token_dependency
+from src.app.schemas.auth_schemas import TokenSchema
 
 router = APIRouter(tags=["verify"])
 
 
-@router.post("/verify/", status_code=status.HTTP_200_OK)
-async def verify(request: ReportRequest):
+@router.post("/verify/")
+async def verify(
+    file: UploadFile, user_id: int = Form(...), token: str = Form(...)
+) -> dict:
     """Проксирует запрос верифиацию по пути к файлу с проверкой токена."""
-    async with httpx.AsyncClient() as client:
-        token_response = await client.post(
-            url=f"{settings.url.auth}/check_token/", json=request.token.model_dump()
-        )
-        if token_response.status_code != status.HTTP_200_OK:
-            raise HTTPException(
-                status_code=token_response.status_code,
-                detail=token_response.json()["detail"],
-            )
-        verify_response = await client.post(
-            url=f"{settings.url.verification}/verify/",
-            json=request.verify.model_dump(),
-            timeout=10,
-        )
+    token_data = await check_token_dependency(token_data=TokenSchema(
+        user_id=user_id,
+        token=token
+    ))
+
+    verify_response = await auth_client.post(
+        endpoint='verify/',
+        data=token_data.model_dump(),
+        files={'file': (
+            file.filename, await file.read(), file.content_type
+        )}
+    )
     return verify_response.json()
