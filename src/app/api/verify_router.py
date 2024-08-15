3f9fa5495f967151de03958763d1f6eafcc90ef5
Aleksey Panin <lllrikitikilll@yandex.ru>
2024-08-15 20:18:45 +0000

    Обновлен хендлер send_data отправка file

--- src/app/api/verify_router.py
+++ src/app/api/verify_router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Form, UploadFile
+from fastapi import APIRouter, File, Form, UploadFile
 
 from src.app.client import auth_client
 from src.app.dependency.auth_dependency import check_token_dependency
@@ -8,16 +8,16 @@ router = APIRouter(tags=["verify"])
 
 
 @router.post("/verify/")
-async def verify(
-    file: UploadFile, user_id: int = Form(...), token: str = Form(...)
+async def send_data(
+    file: UploadFile = File(...), user_id: int = Form(...), token: str = Form(...)
 ) -> dict:
-    """Проксирует запрос верифиацию по пути к файлу с проверкой токена."""
-    token_data = await check_token_dependency(
-        token_data=TokenSchema(user_id=user_id, token=token)
-    )
-    verify_response = await auth_client.post(
+    """Отправляет данные на серверный эндпоинт /verify/."""
+    await check_token_dependency(TokenSchema(user_id=user_id, token=token))
+
+    response = await auth_client.post(
         endpoint="verify/",
-        data=token_data.model_dump(),
         files={"file": (file.filename, await file.read(), file.content_type)},
+        data={"user_id": user_id, "token": token},
     )
-    return verify_response.json()
+
+    return response.json()
