ff8bcc0a6a0bae37c51746bfb223ff3bb25e930a
Aleksey Panin <lllrikitikilll@yandex.ru>
2024-08-15 20:18:07 +0000

    Настройка тестов

--- src/tests/test_api_gateway.py
+++ src/tests/test_api_gateway.py
@@ -78,26 +78,21 @@ async def test_transactions(client, mock_transaction, mock_auth):
         "transaction": {"user_id": 1, "amount": 100, "operation": "debit"},
         "token_data": {"user_id": 1, "token": "valid_token"},
     }
-
-    mock_auth.return_value = Mock(TokenSchema(**transaction_data["token_data"]))
+    mock_auth.return_value = Mock(status_code=status.HTTP_200_OK)
     mock_transaction.return_value = Mock(
         status_code=status.HTTP_200_OK, json=lambda: {"message": "Операция выполнена"}
     )
 
     response = client.post("/transaction/", json=transaction_data)
 
-    transaction_data_copy = transaction_data["transaction"]
-    transaction_data_copy["user_id"] = transaction_data["token_data"]["user_id"]
-    transaction_data_copy["operation"] = transaction_data_copy["operation"].lower()
-
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == {"message": "Операция выполнена"}
-    # mock_auth.assert_awaited_once_with(
-    #     endpoint="check_token/", json=transaction_data["token_data"]
-    # )
-    # mock_transaction.assert_awaited_once_with(
-    #     endpoint="create_transaction/", json=transaction_data_copy
-    # )
+    mock_auth.assert_awaited_once_with(
+        endpoint="check_token/", json=transaction_data["token_data"]
+    )
+    mock_transaction.assert_awaited_once_with(
+        endpoint="create_transaction/", json=transaction_data['transaction']
+    )
 
 
 async def test_report(client, mock_transaction, mock_auth):
@@ -105,33 +100,32 @@ async def test_report(client, mock_transaction, mock_auth):
     from_date = datetime.datetime.now().isoformat()
     to_date = datetime.datetime.now().isoformat()
     report_query = {
-        "query": {
+        "report": {
             "user_id": 1,
             "from_date": from_date,
             "to_date": to_date,
         },
-        "token": {"user_id": 1, "token": "valid_token"},
+        "token_data": {
+            "user_id": 1,
+            "token": "valid_token"
+        },
     }
-    mock_auth.return_value = Mock(
-        status_code=status.HTTP_200_OK, json=lambda: {"message": "Операция выполнена"}
-    )
+    mock_auth.return_value = Mock(status_code=status.HTTP_200_OK)
     mock_transaction.return_value = Mock(
         status_code=status.HTTP_200_OK, json=lambda: {"message": "Операция выполнена"}
     )
 
-    response = client.post("/report/", json=report_query)
-    report_query_copy = report_query["query"]
-    report_query_copy["user_id"] = report_query["token"]["user_id"]
+    response = client.post("report/", json=report_query)
 
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == {"message": "Операция выполнена"}
     # Проверка что запрос проверки токена выполнялся
     mock_auth.assert_awaited_once_with(
-        endpoint="check_token/", json=report_query["token"]
+        endpoint="check_token/", json=report_query["token_data"]
     )
     # Проверка что запрос отчета выполнился
     mock_transaction.assert_awaited_once_with(
-        endpoint="get_report/", json=report_query_copy
+        endpoint="get_report/", json=report_query["report"]
     )
 
 
@@ -142,8 +136,15 @@ async def test_report_error(client, mock_transaction, mock_auth):
     Транзакция не пройдет при отсутствии валидного токена.
     """
     transaction_data = {
-        "transaction": {"user_id": 1, "amount": 100, "operation": "debit"},
-        "token": {"user_id": 1, "token": "invalid_token"},
+        "transaction": {
+            "user_id": 1,
+            "amount": 100,
+            "operation": "debit"
+        },
+        "token_data": {
+            "user_id": 1,
+            "token": "invalid_token"
+        },
     }
     mock_auth.return_value = Mock(
         status_code=status.HTTP_401_UNAUTHORIZED,
@@ -154,12 +155,10 @@ async def test_report_error(client, mock_transaction, mock_auth):
     response = client.post("/transaction/", json=transaction_data)
 
     assert response.status_code == status.HTTP_401_UNAUTHORIZED
-    assert response.json() == {
-        "detail": {"detail": "Неверные данные пользователя или токен"}
-    }
+    assert response.json()['detail'] == 'Неверные данные пользователя или токен'
     # Проверка что запрос проверки токена выполнялся
     mock_auth.assert_awaited_once_with(
-        endpoint="check_token/", json=transaction_data["token"]
+        endpoint="check_token/", json=transaction_data["token_data"]
     )
     # Проверка что запрос транзакции не выполнялся
     mock_transaction.assert_not_awaited()
