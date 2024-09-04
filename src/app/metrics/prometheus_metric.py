b992b4b7f97dd11f2756850a0d5e7345e7124a39
Aleksey Panin <lllrikitikilll@yandex.ru>
2024-09-04 14:41:12 +0000

    Добавлены  настройки метрики

---
+++ src/app/metrics/prometheus_metric.py
@@ -0,0 +1,17 @@
+from typing import Final
+
+from prometheus_client import Histogram, Counter
+
+
+SERVICE_PREFIX: Final[str] = 'gateway'
+REQUEST_COUNT = Counter(
+    name=f'{SERVICE_PREFIX}_request_count',
+    documentation='Total number of requests',
+    labelnames=['method', 'service', 'endpoint', 'status'],
+)
+REQUEST_DURATION = Histogram(
+    name=f'{SERVICE_PREFIX}_request_duration',
+    documentation='Time spent processing request',
+    labelnames=['method', 'service', 'endpoint'],
+    buckets=(0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5, 10),
+)
