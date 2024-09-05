771160f3f3c1c52f3ae60939a8725d159384a2f9
Aleksey Panin <lllrikitikilll@yandex.ru>
2024-09-05 17:40:21 +0000

    Добавлены метрики

--- src/app/main.py
+++ src/app/main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI, Request
 import time
+
+from fastapi import FastAPI, Request
 from opentracing import (
     InvalidCarrierException,
     SpanContextCorruptedException,
@@ -7,13 +8,13 @@ from opentracing import (
     propagation,
     tags,
 )
+from prometheus_client import make_asgi_app
+
 from src.app.api.auth_router import router as auth_router
 from src.app.api.healthz.healthz_router import router as healthz_router
 from src.app.api.transaction_router import router as transaction_router
 from src.app.api.verify_router import router as verify_router
-from prometheus_client import make_asgi_app
 from src.app.metrics.prometheus_metric import REQUEST_COUNT, REQUEST_DURATION
-
 
 app = FastAPI()
 metrics_app = make_asgi_app()
@@ -25,29 +26,31 @@ app.include_router(verify_router)
 app.include_router(healthz_router)
 
 
-def get_service_name(path: str):
-    auth_prefix = '/api/apanin-auth'
-    transactions_prefix = '/api/apanin-transaction'
-    if path.startswith(auth_prefix):
-        service_name = 'auth-service'
-    elif path.startswith(transactions_prefix):
-        service_name = 'transactions-service'
-    else:
-        service_name = 'None'
-    return service_name
+def get_service_name(path: str) -> str:
+    """Получение имени сервиса по его пути."""
+    service_mapping = {
+        "auth-service": ["/api/auth", "/api/registration"],
+        "transactions-service": ["/api/transaction", "/api/report"],
+        "verify-service": ["/api/verify"],
+    }
+
+    for service_name, paths in service_mapping.items():
+        if any(sp in path for sp in paths):
+            return service_name
+
+    return "None"
 
 
 @app.middleware("http")
 async def metrics_middleware(request: Request, call_next):
+    """Middleware для сбора метрик сервиса."""
     start_time = time.time()
     response = await call_next(request)
     process_time = time.time() - start_time
 
     service_name = get_service_name(request.url.path)
     REQUEST_DURATION.labels(
-        method=request.method,
-        service=service_name,
-        endpoint=request.url.path
+        method=request.method, service=service_name, endpoint=request.url.path
     ).observe(process_time)
     REQUEST_COUNT.labels(
         method=request.method,
@@ -61,11 +64,18 @@ async def metrics_middleware(request: Request, call_next):
 
 @app.middleware("http")
 async def tracing_middleware(request: Request, call_next):
+    """Трассирование запроса через разные сервисы."""
     path = request.url.path
-    if path.startswith('/up') or path.startswith('/ready') or path.startswith('/metrics'):
+    if (  # noqa: WPS337
+        path.startswith("/healthz/live")
+        or path.startswith("/healthz/ready")  # noqa: W503
+        or path.startswith("/metrics")  # noqa: W503
+    ):
         return await call_next(request)
     try:
-        span_ctx = global_tracer().extract(propagation.Format.HTTP_HEADERS, request.headers)
+        span_ctx = global_tracer().extract(
+            propagation.Format.HTTP_HEADERS, request.headers
+        )
     except (InvalidCarrierException, SpanContextCorruptedException):
         span_ctx = None
     span_tags = {
@@ -74,7 +84,7 @@ async def tracing_middleware(request: Request, call_next):
         tags.HTTP_URL: str(request.url),
     }
     with global_tracer().start_active_span(
-        f"gateway_{request.method}_{path}",
+        f"apanin_gateway_{request.method}_{path}",
         child_of=span_ctx,
         tags=span_tags,
     ) as scope:
--- src/app/metrics/prometheus_metric.py
+++ src/app/metrics/prometheus_metric.py
@@ -1,17 +1,16 @@
 from typing import Final
 
-from prometheus_client import Histogram, Counter
+from prometheus_client import Counter, Histogram
 
-
-SERVICE_PREFIX: Final[str] = 'gateway'
+SERVICE_PREFIX: Final[str] = "apanin_gateway"
 REQUEST_COUNT = Counter(
-    name=f'{SERVICE_PREFIX}_request_count',
-    documentation='Total number of requests',
-    labelnames=['method', 'service', 'endpoint', 'status'],
+    name=f"{SERVICE_PREFIX}_request_count",
+    documentation="Total number of requests",
+    labelnames=["method", "service", "endpoint", "status"],
 )
 REQUEST_DURATION = Histogram(
-    name=f'{SERVICE_PREFIX}_request_duration',
-    documentation='Time spent processing request',
-    labelnames=['method', 'service', 'endpoint'],
+    name=f"{SERVICE_PREFIX}_request_duration",
+    documentation="Time spent processing request",
+    labelnames=["method", "service", "endpoint"],
     buckets=(0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5, 10),
 )
